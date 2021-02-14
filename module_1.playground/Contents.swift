46b08eeb5f39415fbdf6cf8b16d344add7cb7b2c
boburDev <negmatov1907@gmail.com>
2021-02-15 01:19:47 +0500

    basic syntax lesson finished

--- module_1.playground/Contents.swift
+++ module_1.playground/Contents.swift
@@ -1,5 +1,74 @@
 import UIKit
 
-var name = "Boburmirzo"
+//Comment
 
-print(name)
+//    My first comment
+/*
+    My first comment
+*/
+// Semicalon isnot important it is optional
+
+
+
+// Identifiers ->
+/*
+    A~Z or a~z
+    
+    _ after this we can use numbers and string
+ 
+    symbols and numbers are not allowed for starting varaible name
+*/
+
+
+
+// Whitespace
+
+var apple = 5
+var orange = 12
+
+var fruits = apple + orange // correct
+//var fruits = apple +orange // isnot correct
+
+// Literals
+
+123
+[1,2,3]
+""
+
+// Printing in Swift
+
+print("Apple", "Windows", "Linux")
+print("Apple", "Windows", "Linux", separator: "%")
+print("Apple", "Windows", "Linux", separator: "%", terminator: " :)\n")
+
+
+
+
+
+// TASKS
+
+//task-1
+
+var name = "Boburmirzo"
+var surname = "Negmatov"
+print(name + surname)
+
+//task-2
+
+// printing message
+print("Service: PDP online")
+
+// task-3
+
+print("PDP Online Platform", terminator: " #\n")
+
+// task-4
+
+print("PDP", "Online", "Platform", separator: "||", terminator: " #")
+
+//task-5
+
+var str1 = "pdp"
+var str2 = "online"
+//var str = str1 + " " +str2 // problem is in whitespacing
+//print(str)
