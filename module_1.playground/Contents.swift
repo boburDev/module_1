970d870016c1ca49593121018529139c3fcf4f89
boburDev <negmatov1907@gmail.com>
2021-02-15 08:47:16 +0500

    3tasks left

--- module_1.playground/Contents.swift
+++ module_1.playground/Contents.swift
@@ -1,2 +1,21 @@
 import UIKit
 
+//Type Annotation -> o'zgaruvchining tyoini oldindan belgilab quyosh
+
+var varA:Int = 1
+var subject:String = "Math"
+
+//Naming Variables -> faqat swift da majud bulgan imkoniyat o'garuvchilarni hohlagan tilda yozish imkoniyati
+
+var Привет = "Hello"
+var こんにちは = "Hello"
+var 안녕하세요 = "Hello"
+
+// Printing Variables
+
+var varX = "PDP Online"
+var varY = 1000.00
+
+var str = "Value of \(varX) more than \(varY) millions"
+
+//print(str)
