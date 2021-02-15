9d51e433c322323419eea574bdb6b3efc7ab31f0
boburDev <negmatov1907@gmail.com>
2021-02-15 08:47:43 +0500

    3 tasks left

--- module_1.playground/Contents.swift
+++ module_1.playground/Contents.swift
@@ -19,3 +19,87 @@ var varY = 1000.00
 var str = "Value of \(varX) more than \(varY) millions"
 
 //print(str)
+
+
+// Let vs Var
+
+var language = "English"
+language = "Russian"
+
+let name = "Boburmirzo"
+//name = "Behruz" // bunaqa ishlatish mumkin emas let bu constant
+
+//print(language)
+//print(name)
+
+
+// Boolean
+
+let isEmpty = true
+let canGo = false
+
+
+// Tuples
+let httpError = (404, "Not Found")
+
+//way-1
+let (statusCode1,statusMessage) = httpError
+//print("status: \(statusCode1), message: \(statusMessage)")
+
+
+//way-2
+let (statusCode2, _) = httpError
+//print("status: \(statusCode1)")
+
+
+//way-3
+//print("status: \(httpError.0), message: \(httpError.1)")
+
+
+//way-4
+let httpStatus = (statusCode:200, statusMessage:"ok")
+//print("status: \(httpStatus.statusCode), message: \(httpStatus.statusMessage)")
+
+
+// Optional
+
+let possiableNumber = "123"
+let convertNumber = Int(possiableNumber)
+
+//print(convertNumber ?? possiableNumber)
+//print(convertNumber!)
+
+
+// nil
+
+var isNil:Int? = 44
+isNil = nil
+//print(isNil)
+
+
+//TASKS
+
+//task-1
+
+let num1:Double = 20
+let num2 = 20.20
+let num3 = 2020.20
+
+let avr = (num1 + num2 + num3) / 3
+
+//task-2
+
+var a:Double = 11
+var b = 11.11
+var c = b
+
+//print("a: \(Int(a)), b: \(b)")
+
+b = a
+a = c
+
+//print("a: \(a), b: \(Int(b))")
+
+
+//task-3
+
