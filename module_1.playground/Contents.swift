422816df67a17b518a8f5a91dc437350ac5b5e4a
boburDev <negmatov1907@gmail.com>
2021-03-28 21:42:50 +0500

    .

--- module_1.playground/Contents.swift
+++ module_1.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var str = "Hello, playground"
+// Optional
+
+var perhapsInt:Int?
+var myString:String?
+
+myString = "juba"
+
+if myString != nil {
+    print(myString!)
+}else{
+    print("string is not defined")
+}
+
+
